f2dc9da082e791f9a26f996431cd66579e553208
SMaraia <selio@me.com>
2016-02-25 02:32:43 -0500

    Tower rotates to touch location properly, shooting to come later

--- SKTowerDefense/GameScene.swift
+++ SKTowerDefense/GameScene.swift
@@ -8,38 +8,89 @@
 
 import SpriteKit
 
+
 class GameScene: SKScene {
+    
+    let tower = SKSpriteNode(imageNamed:"Spaceship")
+    
+    var lastUpdateTime: NSTimeInterval = 0
+    var dt: NSTimeInterval = 0
+    
+    var rotDir: CGPoint = CGPoint.zero
+    var lastTouchLocation = CGPoint.zero
+    
+    override init(size: CGSize){
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        tower.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        tower.xScale = 0.5
+        tower.yScale = 0.5
+        addChild(tower)
+        
+
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
+        if(touches.first != nil) {
+            if let touchLocation = touches.first?.locationInNode(self) {
+                sceneTouched(touchLocation)
+            }
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if(touches.first != nil) {
+            if let touchLocation = touches.first?.locationInNode(self) {
+                sceneTouched(touchLocation)
+            }
+            
+        }
+    }
+    
+    func sceneTouched(touchLocation: CGPoint)
+    {
+        lastTouchLocation = touchLocation
+        let towerToTouch = touchLocation - tower.position
+        let normalizedTowerTouch = towerToTouch.normalized()
+        rotDir = normalizedTowerTouch
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        }
+        else
+        {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        
+        rotateTower(rotDir, rotateRadiansPerSec: 4.0 * π)
     }
+    
+    func rotateTower(direction: CGPoint, rotateRadiansPerSec: CGFloat)
+    {
+        let shortAngle = shortestAngleBetween(tower.zRotation, angle2: direction.angle)
+        
+        var rotAmt = rotateRadiansPerSec * CGFloat(dt)
+        
+        rotAmt = min(rotAmt, abs(shortAngle))
+        
+        tower.zRotation += rotAmt * shortAngle.sign()
+    }
+
 }
+
+
